f7b5eb603e71846281b724cf820e1305e03ce2e6
Anas Shahid <muhammad.shahid@mail.mcgill.ca>
2019-10-20 12:59:39 -0400

    testing build

---
+++ build/precache-manifest.df1fa9bafc352d3b778389a3b938cc17.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "6de25a0be6286ba78704",
+    "url": "/static/css/main.71a0450c.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd61aba7f4c048ccaedfa66354406f6f",
+    "url": "/static/media/Fall_2019_V1.dd61aba7.png"
+  },
+  {
+    "revision": "8a5c365e329467c506c3",
+    "url": "/static/js/2.5289e5f8.chunk.js"
+  },
+  {
+    "revision": "35523bbcb9828de576ee33a9cf69e6e0",
+    "url": "/static/media/Defilia.35523bbc.jpg"
+  },
+  {
+    "revision": "8e3c83ba6154eaabf4d6792938583f65",
+    "url": "/static/media/epts.8e3c83ba.jpg"
+  },
+  {
+    "revision": "8c18d4ce5eef8edfb8ad6538002056fb",
+    "url": "/static/media/Logo-Text.8c18d4ce.png"
+  },
+  {
+    "revision": "6de25a0be6286ba78704",
+    "url": "/static/js/main.dd813cb3.chunk.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "228afd7147f835b318b1978d5f4c9383",
+    "url": "/static/media/dawn.228afd71.jpg"
+  },
+  {
+    "revision": "8649af831cc6d990b4c9c728a228ab63",
+    "url": "/static/media/facebook-cover.8649af83.png"
+  },
+  {
+    "revision": "28e45ff94329db9aafa7e67d76c7116b",
+    "url": "/static/media/Salar.28e45ff9.jpg"
+  },
+  {
+    "revision": "8a5c365e329467c506c3",
+    "url": "/static/css/2.401474f0.chunk.css"
+  },
+  {
+    "revision": "99d937d059ea6d12f72ed3bdf57747ab",
+    "url": "/index.html"
+  }
+];
