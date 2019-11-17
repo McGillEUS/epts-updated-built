905558c57c3eaaf0ef0c89b4d2a7c6bda41118bc
Anas Shahid <muhammad.shahid@mail.mcgill.ca>
2019-11-17 15:34:06 -0500

    Build folder updated

---
+++ build/precache-manifest.0d00c0d4a35244dbd695a64a85587a82.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "d88e39717de112aec7a3c54c7ed09324",
+    "url": "/static/media/Denis.d88e3971.jpg"
+  },
+  {
+    "revision": "09081e72292c8a18c39d",
+    "url": "/static/css/main.fec4d809.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "039d74573b6261e0a1760a88f5d13b4a",
+    "url": "/static/media/Fall_2019_V1.039d7457.png"
+  },
+  {
+    "revision": "a716e9a8a4f6c665c0df",
+    "url": "/static/js/2.45779dd0.chunk.js"
+  },
+  {
+    "revision": "8e3c83ba6154eaabf4d6792938583f65",
+    "url": "/static/media/epts.8e3c83ba.jpg"
+  },
+  {
+    "revision": "8c18d4ce5eef8edfb8ad6538002056fb",
+    "url": "/static/media/Logo-Text.8c18d4ce.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "228afd7147f835b318b1978d5f4c9383",
+    "url": "/static/media/dawn.228afd71.jpg"
+  },
+  {
+    "revision": "8649af831cc6d990b4c9c728a228ab63",
+    "url": "/static/media/facebook-cover.8649af83.png"
+  },
+  {
+    "revision": "23578cdc08437b63e15bbf84b0f6a0b6",
+    "url": "/static/media/Deifilia.23578cdc.jpg"
+  },
+  {
+    "revision": "09081e72292c8a18c39d",
+    "url": "/static/js/main.924fc4ee.chunk.js"
+  },
+  {
+    "revision": "e8d19a8dfafc4d1d3c74956f2e5760ed",
+    "url": "/static/media/Abdul.e8d19a8d.jpg"
+  },
+  {
+    "revision": "c2b45675aef2a6c511f8d222152b5232",
+    "url": "/static/media/Dylan.c2b45675.jpg"
+  },
+  {
+    "revision": "85520c860cdb447118e369d87c642341",
+    "url": "/static/media/Hansen.85520c86.jpg"
+  },
+  {
+    "revision": "4a62fce38ae2268ca807a40fa136ef9a",
+    "url": "/static/media/Itai.4a62fce3.jpg"
+  },
+  {
+    "revision": "3706ef4c388a5af1fb74b2e1c1f3c6b2",
+    "url": "/static/media/George.3706ef4c.jpg"
+  },
+  {
+    "revision": "d7eee167556fdb99429c339e451a0ffb",
+    "url": "/static/media/Judy.d7eee167.jpg"
+  },
+  {
+    "revision": "55950892ceeee17af00998de406bf940",
+    "url": "/static/media/Mukit.55950892.jpg"
+  },
+  {
+    "revision": "6b033b1c66a84e663c084d4898373e6e",
+    "url": "/static/media/Nicolas.6b033b1c.jpg"
+  },
+  {
+    "revision": "8f16f8ad8256242872b3db1f363b6f4a",
+    "url": "/static/media/Roey.8f16f8ad.jpg"
+  },
+  {
+    "revision": "2f83e578b13d68abe0db2a84bca81751",
+    "url": "/static/media/Salar.2f83e578.jpg"
+  },
+  {
+    "revision": "c0c0c5314e04f59314d438d221a2651b",
+    "url": "/static/media/Sepher.c0c0c531.jpg"
+  },
+  {
+    "revision": "9cf38aa9eaaeab59e7eac0cfcb03c5bf",
+    "url": "/static/media/Sinan.9cf38aa9.jpg"
+  },
+  {
+    "revision": "a716e9a8a4f6c665c0df",
+    "url": "/static/css/2.958e32a9.chunk.css"
+  },
+  {
+    "revision": "7fc041c844fc7eafc5d86777e6304421",
+    "url": "/index.html"
+  }
+];
